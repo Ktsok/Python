befd3cade715130e8f6e5494d0748e0d957f010c
Ktsok <140635192+Ktsok@users.noreply.github.com>
2025-07-02 21:48:15 +0300

    Add files via upload

---
+++ chapter01/05_input_demo.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+print ("Hello", name)
+
+year_of_birth = input("Please enter your year of birth: ")
+year_of_birth = int(year_of_birth)  # Convert input to integer
+print("You are", 2025 - year_of_birth, "years old")
