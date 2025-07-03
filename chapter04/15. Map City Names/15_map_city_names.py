629bfcebefd9d83c9d9ab742e1aaf48852b6a3b6
Ktsok <140635192+Ktsok@users.noreply.github.com>
2025-07-03 22:50:00 +0300

    Delete chapter04 directory

--- chapter04/15. Map City Names/15_map_city_names.py
+++
@@ -1,9 +0,0 @@
-# List of city names
-cities = ["london", "paris", "barcelona", "athens"]
-
-# Using map and a lambda function to capitalize each city name
-# The title() function is used to convert the first character of each word to uppercase and the rest to lowercase
-cap_cities = list(map(lambda city: city.title(), cities))
-
-# Printing the list of capitalized city names
-print("Capitalized Cities:", cap_cities)
