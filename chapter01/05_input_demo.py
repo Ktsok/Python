680c4af17c130f05596593fa90c18018277b2c5c
Ktsok <140635192+Ktsok@users.noreply.github.com>
2025-07-03 17:46:02 +0300

    Delete chapter01 directory

--- chapter01/05_input_demo.py
+++
@@ -1,6 +0,0 @@
-name = input("Please enter your name: ")
-print ("Hello", name)
-
-year_of_birth = input("Please enter your year of birth: ")
-year_of_birth = int(year_of_birth)  # Convert input to integer
-print("You are", 2025 - year_of_birth, "years old")
